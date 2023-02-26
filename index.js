98f68ffbcb0088ab6b9ca28629b12cb1d6b00ee8
mirakernel <mirakernel@disroot.org>
2023-02-27 01:53:22 +0300

    Принимает видео, фото и текст, с гифками проблема

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const { Telegraf } = require('telegraf');
+const rateLimit = require('telegraf-ratelimit');
 
 const bot = new Telegraf('6228082361:AAHphN_mLTRP7qG-Wi3GCbMyqKWL9Ew00rk');
+const MODERATOR_CHAT_ID = '498989662';
+
+const limitConfig = {
+    window: 5000, // Время, в течение которого происходит ограничение (в миллисекундах)
+    limit: 1, // Максимальное количество сообщений за время window
+    keyGenerator: (ctx) => ctx.from.id, // Генератор ключей для ограничения по пользователю
+    onLimitExceeded: (ctx, next) => ctx.reply('Пожалуйста, подождите 5 секунд, прежде чем отправлять новое сообщение.'), // Сообщение, отправляемое при превышении лимита
+  };
+  
+  const limitMiddleware = rateLimit(limitConfig);
+  bot.use(limitMiddleware);
 
 // Обработка команды /start
 bot.start((ctx) => ctx.reply('Добро пожаловать! Отправьте сообщение, чтобы отправить предложку.'));
@@ -11,16 +23,49 @@ bot.command('getchatid', (ctx) => {
     ctx.reply(`Ваш chat ID: ${chatId}`);
   });
 
+
 // Обработка сообщений
-bot.on('message', (ctx) => {
-  const message = ctx.message.text;
-
-  // Отправка сообщения модератору
-  bot.telegram.sendMessage('498989662', `Получено предложение: ${message}`);
-
+bot.on('photo', (ctx) => {
+    const photo = ctx.message.photo[0].file_id;
+    const user = ctx.message.from.username;
+    const caption = ctx.message.caption || '';
+  
+    // Отправляем сообщение модератору
+    ctx.telegram.sendPhoto(MODERATOR_CHAT_ID, photo, {
+        caption: `\n${caption}\n\nПредложил(а) - @${user}\nПредложить пост - @AranaraFrittersBot`,
+    });
+  
+    // Обрабатываем фото
+    // ...
   // Ответ пользователю
   ctx.reply('Спасибо за предложку! Мы ее рассмотрим.');
-});
+  });
+  
+  bot.on('video', (ctx) => {
+    const video = ctx.message.video.file_id;
+    const user = ctx.message.from.username;
+    const caption = ctx.message.caption || '';
+  
+    // Отправляем сообщение модератору
+    ctx.telegram.sendVideo(MODERATOR_CHAT_ID, video, {
+        caption: `\n${caption}\n\nПредложил(а) - @${user}\nПредложить пост - @AranaraFrittersBot`,
+    });
+  
+  // Ответ пользователю
+  ctx.reply('Спасибо за предложку! Мы ее рассмотрим.');
+  });
+  
+  bot.on('text', (ctx) => {
+    const text = ctx.message.text;
+    const user = ctx.message.from.username;
+  
+    // Отправляем сообщение модератору
+    const message = `${text}\n\nПредложил(а) - @${user}\nПредложить пост - @AranaraFrittersBot`;
+    ctx.telegram.sendMessage(MODERATOR_CHAT_ID, message);
+  
+    // Ответ пользователю
+    ctx.reply('Спасибо за предложку! Мы ее рассмотрим.');
+  });
 
 // Запуск бота
 bot.launch();
